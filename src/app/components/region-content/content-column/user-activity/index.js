2ebe3ddb2681a205c877368436f2912c97ed829e
gariasf <gariasfa7@alumnes.ub.edu>
2018-03-14 16:13:05 +0100

    Refactor

--- src/app/components/region-content/content-column/user-activity/index.js
+++ src/app/components/region-content/content-column/user-activity/index.js
@@ -2,14 +2,14 @@ import { h } from 'preact'
 
 import ContentHeader from '../../../shared/content-header'
 import NewLinkForm from './new-link-form'
-import History from './history'
+import HistoryTable from './history-table'
 
 export default function UserActivity () {
   return (
     <div>
       <ContentHeader title='Activity' />
       <NewLinkForm />
-      <History />
+      <HistoryTable />
     </div>
   )
 }
