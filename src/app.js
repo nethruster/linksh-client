ac32ad1665b743bf3ebd1b9fc572c7513a94dc1c
Guillem Arias <accounts@gariasf.com>
2018-08-18 14:54:45 +0200

    Wrapp app with store provider

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import { h, render } from 'preact'
+import { Provider } from 'unistore/preact'
+import 'preact/debug'
+
+import { store } from 'store'
 
 import './app.scss'
 
 const mountPoint = document.querySelector('#linksh')
 
-render(<div>Linksh</div>, mountPoint, mountPoint.lastElementChild)
+const App = () => (
+  <Provider store={store}>
+    <div>lknsh</div>
+  </Provider>
+)
+
+render(<App />, mountPoint, mountPoint.lastElementChild)
