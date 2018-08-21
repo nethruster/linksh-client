748b93f2bb61b0b2f475b4000991aaa6a43b420e
Guillem Arias <gariasfa7@almneses.ub.edu>
2018-08-21 16:53:56 +0200

    Tweak button and icon components

--- src/views/shared/icon/index.js
+++ src/views/shared/icon/index.js
@@ -2,12 +2,12 @@ import { h } from 'preact'
 
 import icons from './icons'
 
-export default function Icon({ name, size = '12', marginRight, color }) {
+export default function Icon({ name, size = '20', marginRight, color }) {
   return (
     <svg
       width={size}
       height={size}
-      viewBox="0 0 1024 1024"
+      viewBox="0 0 24 24"
       class={marginRight ? 'marginRight' : ''}
       style={`min-width: ${size}; min-height: ${size}`}
     >
